2ab3d8603343e8a15514670ca7c46ebe5ad703ae
Wolfeduck90 <djevanzwolfe@gmail.com>
2025-07-09 10:17:52 +0200

    Update convert_tflite.py

--- src/convert_tflite.py
+++ src/convert_tflite.py
@@ -1,6 +1,16 @@
 import tensorflow as tf
-import os
 
-# --- CONFIGURATION ---
-MODEL_INPUT_PATH = "../model/recyclable_model.h5"
-MODEL_OUTPUT_PATH = "../model/recyclable_model.tflite"
+# Paths
+INPUT_MODEL = "../model/recyclable_model.h5"
+OUTPUT_TFLITE = "../model/recyclable_model.tflite"
+
+# Load and convert
+model = tf.keras.models.load_model(INPUT_MODEL)
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
+
+# Save TFLite model
+with open(OUTPUT_TFLITE, "wb") as f:
+    f.write(tflite_model)
+
+print(f"✅ TFLite model saved: {OUTPUT_TFLITE}")
